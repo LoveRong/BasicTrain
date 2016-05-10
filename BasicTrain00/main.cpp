361d2a6680f0eaaf3696eac907a5ef3a16eceb38
LoveRong <ykmzy@qq.com>
2016-05-11 07:52:57 +0800

    添加归并插入算法

--- BasicTrain00/main.cpp
+++ BasicTrain00/main.cpp
@@ -2,21 +2,24 @@
 #include <list>
 using namespace std;
 
+const int gLen = 9;
+
 int BubbleSort(int* pArray);
 int InsertSort(int* pArray);
-int MergeSort(int* pArray);
+int MergeSort(int* pArray, int len);
 
 // 排序
 int Sort()
 {
   cout<<"Mirage***排序练习****"<<endl;
- 
-  int* intArray = new int[8] {20, 3, 1, 7, 4, 90, 0, 4};
+
+  int* intArray = new int[gLen] {20, 3, 1, 7, 4, 90, 0, 4, 6};
   //BubbleSort(intArray);
-  InsertSort(intArray);
+  //InsertSort(intArray);
+  MergeSort(intArray, gLen);
 
   cout<<"排序后生成的序列: ";
-  for (int i = 0; i < 8; i++)
+  for (int i = 0; i < gLen; i++)
   {
 	  cout << intArray[i] << " ";
   }
@@ -28,9 +31,9 @@ int Sort()
 // 冒泡排序
 int BubbleSort(int* pArray)
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < gLen; i++)
 	{
-		for (int j = i; j < 8; j++)
+		for (int j = i; j < gLen; j++)
 		{
 			if (pArray[i] > pArray[j])
 			{
@@ -45,7 +48,7 @@ int BubbleSort(int* pArray)
 // 插入排序
 int InsertSort(int* pArray)
 {
-	for (int i = 1; i < 8;i++)
+	for (int i = 1; i < gLen;i++)
 	{
 		int key = pArray[i];
 		int j = i - 1;
@@ -59,26 +62,43 @@ int InsertSort(int* pArray)
 	return 0;
 }
 // 归并排序
-int MergeSort(int* pArray)
+int MergeSort(int* pArray, int len)
 {
+  int* a = pArray;
+  int* b = new int[len];
 
-	return 0;
-}
-// 希尔排序
-int ShellSort(int* pArray, int maxNum)
-{
-	int step = 3;
-	for (int i = 0; i < maxNum / step; i++)
-	{
-		while()
-		{}
-	}
+  for(int seg=1; seg<len; seg+=seg){
+    for(int start=0; start<len; start+=seg+seg){
+      int low=start,mid=min(start+seg,len),high=min(start+seg+seg,len);
+      int k=low;
+      int start1=low,end1=mid;
+      int start2=mid,end2=high;
+      while(start1<end1 && start2<end2)
+        b[k++]=a[start1]<a[start2]?a[start1++]:a[start2++];
+      while (start1<end1) {
+        b[k++]=a[start1++];
+      }
+      while (start2<end2) {
+        b[k++]=a[start2++];
+      }
+    }
+    int* temp = a;
+    a=b;
+    b=temp;
+  }
+  if(a!=pArray){
+    for(int i=0;i<len;i++)
+      b[i]=a[i];
+  b = a;
+  }
+
+  delete[] b;
 	return 0;
 }
 
 int main(int argc, char const *argv[]) {
   Sort();
-	
+
   char ch;
   cin >> ch;
   return 0;
